aa4e5a60a06dd3dc523f5fbe1954f760be1d842d
Jonas <jonas.abelnx@gmail.com>
2025-07-23 23:12:49 +0200

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyDrSwaMpg2TqhaJASg1mf14TQqeVo-DwTs",
+  authDomain: "traveladmin-4931c.firebaseapp.com",
+  databaseURL: "https://traveladmin-4931c-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "traveladmin-4931c",
+  storageBucket: "traveladmin-4931c.appspot.com",
+  messagingSenderId: "55525482776",
+  appId: "1:55525482776:web:ab27c3f0b3745bf16bfe6d",
+  measurementId: "G-GWJGB8SNSM"
+};
