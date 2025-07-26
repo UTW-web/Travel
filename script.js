b9b975e485021713430c6a946ed5973b17314075
UTW-web <jonas.abelnx@gmail.com>
2025-07-26 23:49:15 +0200

    Added picture in About me and in sunset,sunrise

--- script.js
+++ script.js
@@ -101,45 +101,6 @@
     }
   });
 
-// Slideshow functionality 1
-let slideIndex = 0;
-showSlides();
-
-function showSlides() {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";  
-  }
-  slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}    
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " active";
-  setTimeout(showSlides, 2000); // Change image every 2 seconds
-}
-//Slideshow functionality 2
-let currentIndex = 0;
-const allSlides = document.querySelectorAll('.slide');
-
-function switchSlide() {
-  // Remove current from all
-  allSlides.forEach(slide => {
-    slide.classList.remove('current');
-    slide.style.left = '100%';
-  });
-
-  currentIndex = (currentIndex + 1) % allSlides.length;
-
-  // Set next one
-  allSlides[currentIndex].classList.add('current');
-  allSlides[currentIndex].style.left = '0';
-}
-
-setInterval(switchSlide, 3000); // Change every 3 seconds
 
 //Clickable image
   document.addEventListener('click', function(e) {
